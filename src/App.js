ac07fd8a7a8b8a20287820464390cf9f6e3300dd
yeongjunekoh <rhdudwns0@naver.com>
2021-07-16 17:07:45 +0900

    [FEATURE] Generate main page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import "./App.css";
-import NameForm from "./component/form/NameForm";
-import GenderForm from "./component/form/GenderForm/index";
-import { useState, useCallback } from "react";
+
+import MainPage from "./pages/MainPage";
 
 function App() {
-  const [isToggled, setIsToggled] = useState(false);
-
-  const toggleButton = useCallback(() => {
-    setIsToggled((prev) => !prev);
-  }, []);
-
   return (
     <div>
-      <NameForm />
-      <p className="title">성별</p>
-
-      <GenderForm text={"남성"} isToggled={!isToggled} onClick={toggleButton} />
-      <GenderForm text={"여성"} isToggled={isToggled} onClick={toggleButton} />
+      <MainPage />
     </div>
   );
 }
--- src/component/form/NameForm/index.jsx
+++ src/component/form/NameForm/index.jsx
@@ -5,7 +5,7 @@ function NameForm() {
   return (
     <>
       <p className="title">이름</p>
-      <input className="input" defaultValue={"이름을 입력하세요"}></input>
+      <input className="input" placeholder={"이름을 입력하세요"}></input>
     </>
   );
 }
