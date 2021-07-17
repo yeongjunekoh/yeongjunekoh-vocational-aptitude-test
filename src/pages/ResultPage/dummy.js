8e30e89ac7a003e0447f8647b72231f84bac2cf1
yeongjunekoh <rhdudwns0@naver.com>
2021-07-17 16:22:22 +0900

    [FEATURE] Generate result page

---
+++ src/pages/ResultPage/dummy.js
@@ -0,0 +1,35 @@
+export const resultDummyData = {
+  name: "고영준",
+  gender: "남",
+  date: "2021.1.01",
+  result: [
+    {
+      resultCategoryName: "결과",
+      result: 100,
+    },
+    {
+      resultCategoryName: "결과",
+      result: 90,
+    },
+    {
+      resultCategoryName: "결과",
+      result: 70,
+    },
+    {
+      resultCategoryName: "결과",
+      result: 90,
+    },
+    {
+      resultCategoryName: "결과",
+      result: 30,
+    },
+    {
+      resultCategoryName: "결과",
+      result: 30,
+    },
+    {
+      resultCategoryName: "결과",
+      result: 30,
+    },
+  ],
+};
