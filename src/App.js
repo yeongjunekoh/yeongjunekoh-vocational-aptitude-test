2ff93f397e59c12937593d06f6d775e018f79498
yeongjunekoh <rhdudwns0@naver.com>
2021-07-18 21:08:01 +0900

    [FIX] Fix gender form depending on single choice form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
 
 import MainPage from "./pages/MainPage";
-import InspectionPage from "./pages/InspectionPage";
-import EndingPage from "./pages/EndingPage";
-import ResultPage from "./pages/ResultPage";
 
 function App() {
   return (
     <div className="App">
-      <ResultPage />
+      <MainPage />
     </div>
   );
 }
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 
 import "./index.css";
 
@@ -6,38 +6,86 @@ import NameForm from "../../component/form/NameForm";
 import BasicButton from "../../component/button/BasicButton";
 import SingleChoiceForm from "../../component/form/SingleChoiceForm";
 
+const genderListData = [
+  {
+    gender: "남성",
+    isSelected: false,
+  },
+  {
+    gender: "여성",
+    isSelected: false,
+  },
+];
+
 function MainPage() {
-  const [isToggled, setIsToggled] = useState(false);
+  const [genderLIst, setGenderList] = useState(genderListData);
   const [name, setName] = useState("");
 
-  const toggleButton = useCallback(() => {
-    setIsToggled((prev) => !prev);
-  }, []);
+  const verifyCheckedNumber = useMemo(() => {
+    return genderLIst.filter((item) => item.isSelected === true).length;
+  }, [genderLIst]);
+  console.log(genderLIst);
+
+  const handleSelectGender = useCallback(
+    (text, isToggled, index) => {
+      const shouldBlockCheck = verifyCheckedNumber >= 1;
+      const option = { gender: text, isSelected: !isToggled };
+
+      if (shouldBlockCheck && isToggled) {
+        setGenderList((prev) => [
+          ...prev.slice(0, index),
+          { ...option },
+          ...prev.slice(index + 1),
+        ]);
+      }
+      if (shouldBlockCheck && !isToggled) {
+        setGenderList((prev) => [
+          ...prev.slice(0, index).map((item) => {
+            return { gender: item.gender, isSelected: !item.gender };
+          }),
+          { ...option },
+          ...prev.slice(index + 1).map((item) => {
+            return { gender: item.gender, isSelected: !item.gender };
+          }),
+        ]);
+      }
+      if (!shouldBlockCheck && isToggled) {
+      }
+      if (!shouldBlockCheck && !isToggled) {
+        setGenderList((prev) => [
+          ...prev.slice(0, index),
+          { ...option },
+          ...prev.slice(index + 1),
+        ]);
+      }
+    },
+    [verifyCheckedNumber]
+  );
 
   const onClickStartButton = useCallback(() => {
     console.log("시작 할게요");
   }, []);
 
   return (
-    <div className="container">
+    <div className="main-page-container">
       <h1>직업 가치관 검사</h1>
       <NameForm />
-      <p className="title">성별</p>
+      <p className="gender-title">성별</p>
 
-      <SingleChoiceForm
-        text={"남성"}
-        isToggled={!isToggled}
-        onClick={toggleButton}
-      />
-      <SingleChoiceForm
-        text={"여성"}
-        isToggled={isToggled}
-        onClick={toggleButton}
-      />
-
+      {genderLIst.map((item, idx) => {
+        return (
+          <SingleChoiceForm
+            key={idx}
+            index={idx}
+            text={item.gender}
+            isToggled={item.isSelected}
+            onClick={handleSelectGender}
+          />
+        );
+      })}
       <BasicButton
         text="검사 시작"
-        onClick={name === "" ? {} : onClickStartButton}
+        onClick={name === "" ? () => {} : onClickStartButton}
       />
     </div>
   );
