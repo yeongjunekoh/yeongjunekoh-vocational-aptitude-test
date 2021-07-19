5ec519da51c6df4c6a3a08cfa9269dc1c4e94048
yeongjunekoh <rhdudwns0@naver.com>
2021-07-19 23:42:32 +0900

    [FEATURE] Generate inspection question reducer

--- src/modules/questionAnswer.js
+++ src/modules/questionAnswer.js
@@ -4,11 +4,26 @@ export const getData = axios.get(
   "https://www.career.go.kr/inspct/openapi/test/questions?apikey=72612ba54c1decfb085cfe680f85ce3a&q=6&trgetSe=100208"
 );
 
+export const sendData = axios.post(
+  "https://www.career.go.kr/inspct/openapi/test/questions?apikey=72612ba54c1decfb085cfe680f85ce3a&q=6&trgetSe=100208"
+);
+
 const SET_QUESTION_ANSWER = "counter/SET_QUESTION_ANSWER";
+const UPDATE_QUESTION_ANSWER = "counter/UPDATE_QUESTION_ANSWER";
+const DELETE_DATA = "counter/DELETE_DATA";
 
 export const setQuestionAnswer = (data) => ({
   type: SET_QUESTION_ANSWER,
   data: data,
+});
+
+export const updateQuestionAnswer = (data) => ({
+  type: UPDATE_QUESTION_ANSWER,
+  data: data,
+});
+
+export const deleteData = () => ({
+  type: DELETE_DATA,
 });
 
 const initialState = [
@@ -23,6 +38,14 @@ export default function questionAnswer(state = initialState, action) {
   switch (action.type) {
     case SET_QUESTION_ANSWER:
       return [...state, ...action.data];
+    case UPDATE_QUESTION_ANSWER:
+      return [
+        ...state.slice(0, action.data[0].questionNumber),
+        ...action.data,
+        ...state.slice(action.data[action.data.length - 1].questionNumber + 1),
+      ];
+    case DELETE_DATA:
+      return [...initialState];
 
     default:
       return state;
